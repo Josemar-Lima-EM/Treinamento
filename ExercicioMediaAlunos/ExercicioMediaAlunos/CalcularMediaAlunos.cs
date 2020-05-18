73f10386c263eff93e1034e4fd3075dff32034d1
josemar <josemar.lima@escolarmanager.com.br>
2020-05-18 14:00:24 -0300

    exercicio tdd

---
+++ ExercicioMediaAlunos/ExercicioMediaAlunos/CalcularMediaAlunos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioMediaAlunos
+{
+    public class CalcularMediaAlunos<T>
+    {
+        public T calcularMedia(T nota1, T nota2)
+        {
+
+            double numero1 = 0;
+            double numero2 = 0;
+            object resultadoFinal = 0;
+
+            string string1 = Convert.ToString(nota1);
+            string string2 = Convert.ToString(nota2);
+
+            bool testeTipo = double.TryParse(string1, out numero1).Equals(double.TryParse(string2, out numero2));
+
+            if (testeTipo == false) { 
+
+            
+            }
+            return resultadoFinal = (object) nota1 + nota2;
+        }
+       
+    }
+}
---
+++ ExercicioMediaAlunos/ExercicioMediaAlunos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioMediaAlunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com a 1a nota do aluno:");
+            var nota1 = Console.ReadLine();
+            Console.WriteLine("Entre com a 2a nota do aluno:");
+            var nota2 = Console.ReadLine();
+
+            CalcularMediaAlunos<string> calcularMediaAlunos = new CalcularMediaAlunos<string>();
+            object obj2 = new Obj();
+
+            calcularMediaAlunos.calcularMedia(nota1, nota2);
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+            
+
+            
+
+        }
+        public class Obj
+        {
+
+        }
+    }
+}
